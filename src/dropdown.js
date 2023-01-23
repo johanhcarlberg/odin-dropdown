f0f1ecd765ddddb9c64c8f38c726690fa51f3719
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-23 07:37:36 +0100

    Add rendering logic to dropdown

--- src/dropdown.js
+++ src/dropdown.js
@@ -5,6 +5,20 @@ export default class Dropdown {
 
     render() {
         this.dropdownList = document.createElement('ul');
+        this.dropdownList.classList.add('dropdown')
+
+        for (const item of this.items) {
+            const dropdownItem = document.createElement('li');
+            dropdownItem.classList.add('dropdown-item');
+
+            const dropdownLink = document.createElement('a');
+            dropdownLink.classList.add('dropdown-item-link');
+            dropdownLink.href = item.link;
+            dropdownLink.text = item.title;
+            dropdownItem.appendChild(dropdownLink);
+
+            this.dropdownList.appendChild(dropdownItem);
+        }
 
         return this.dropdownList;
     }
