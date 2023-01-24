f7545bf55cf0bcf4d4c766459faf0ae3ac807f25
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-24 15:50:25 +0100

    Add right-aligned dropdown to example

--- example/index.js
+++ example/index.js
@@ -7,8 +7,14 @@ const navLinks = nav.querySelector('ul');
 const dropdown = new Dropdown();
 dropdown.addItem('Dropdown 1', '');
 
+const dropdownLeft = new Dropdown(true);
+dropdownLeft.addItem('Dropdown 1', '');
+
 navLinks.children[0].appendChild(dropdown.render());
 navLinks.children[0].addEventListener('click', () => dropdown.toggle());
+
+navLinks.children[2].appendChild(dropdownLeft.render());
+navLinks.children[2].addEventListener('click', () => dropdownLeft.toggle());
 
 
 
